4c9a616a6b299c299c6d079ed1233436462a8ed4
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2022-10-15 14:11:32 -0700

    Cont.. Getter Funcs

---
+++ getter_funcs2.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+ * get_precision - gets the precision from the format string
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ * Return: new pointer
+*/
+
+char *get_precision(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s != '.')
+		return (s);
+	s++;
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->precision = d;
+	return (s);
+}
+
+/**
+ * get_width - gets the width from the format string
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ * Return: new pointer
+ */
+
+char *get_width(char *s, params_t *params, va_list ap)
+
+	/* should this function use char **s and modify the pointer? */
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
+
+/**
+ * get_print_func - finds the format function
+ * @s: string of the format
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: the number of bytes printed
+ */
+
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
