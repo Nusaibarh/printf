4b34464888f662bb3ee7699b6fce3e013ecfb2ce
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2022-10-18 17:19:57 +0100

    task 0

--- _printf.c
+++ _printf.c
@@ -17,23 +17,23 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-		if (*p != '%')/*checking for the % specifier*/
+		if (*p != '%')
 		{
 			sum += _putchar(*p);
 			continue;
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params)) /* while char at p is flag character */
+		while (get_flag(p, &params))
 		{
-			p++; /* next character */
+			p++; 
 		}
 		p = get_width(p, &params, ap);
 		p = get_precision(p, &params, ap);
@@ -49,5 +49,3 @@ int _printf(const char *format, ...)
 	va_end(ap);
 	return (sum);
 }
-
-
