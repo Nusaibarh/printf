d4399de3dcaa37bc4fc2a2a23adaefcbed17b898
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2022-10-18 22:42:10 +0100

    persisting error

--- main.h
+++ main.h
@@ -17,6 +17,25 @@
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
+/**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if space_flag specified
+ * @hashtag_flag: on if hashtag_flag specified
+ * @zero_flag: on if zero_flag specified
+ * @minus_flag: on if minus_flag specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+
 typedef struct parameters
 {
 	unsigned int unsign			: 1;
@@ -31,25 +50,37 @@ typedef struct parameters
 	unsigned int l_modifier		: 1;
 } params_t;
 
+/**
+ * struct specifier - struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
+ */
 typedef struct specifier
 {
 	char *specifier;
 	int (*f)(va_list, params_t *);
 } specifier_t;
 
+/* _put.c module */
 int _puts(char *str);
 int _putchar(int c);
+
+/* print_functions.c module */
 int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
+
+/* modules */
 int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
 int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
 char *get_width(char *s, params_t *params, va_list ap);
 char *get_precision(char *p, params_t *params, va_list ap);
+
 int print_hex(va_list ap, params_t *params);
 int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
@@ -62,10 +93,14 @@ int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
+
 int _isdigit(int c);
 int _strlen(char *s);
 char *convert(long int num, int base, int flags, params_t *params);
+
 void init_params(params_t *params, va_list ap);
+
+/* _prinf.c module */
 int _printf(const char *format, ...);
 
-#endif
+#endif /*_MAIN_H_*/
